ebf971e9655b2828dbab538ee5eb521a67845693
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-01-21 15:47:05 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("a");
+scanf("\t%d",&a);
+if (a<0)
+{
+printf("\negative");
+else if (a=0)
+{
+printf("\nzero");
+}
+else
+{
+printf("\negative");
+}
+}
