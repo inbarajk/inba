5cbd249d2675a6b44a6fa84793b2f395fdeedaef
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-01-26 16:10:59 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter the value of a:");
+scanf("%d",&a);
+printf("\n enter the value of b:");
+scanf("%d",&b);
+c=a+b;
+printf("\n tha value of%d",c);
+getch();
+}
