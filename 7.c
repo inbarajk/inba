b60aeb0fa9f6c09716dcfb7919a022b40385df14
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-01-26 09:13:49 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+char c;
+printf(“Enter the word",c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+    printf("\n the word is vowel");
+}
+else
+{
+    printf("\n the word is constrant");
+}
+}
