b56f213a7d9528ac1495039b25937615ece72714
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-01-30 16:37:34 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a=0,b=6;
+clrscr()
+    for(int i=a+1;i<b;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+        }
+        getch();
+    }
