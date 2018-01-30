8f576275f9e6806d49172640bef8acb26b5aa822
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-01-30 16:58:19 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b = 1;
+
+    for(a=2; a <= n/2; ++a)
+    {
+        if (n%a == 0)
+        {
+            b =0;
+            break;
+        }
+    }
+    return flag;
+}
+{
+    int a, c, i,b;
+    printf(" two positive integers: ");
+    scanf("%d %d", &a, &c);
+    printf("Prime numbers  %d and %d : ", a, c);
+    for(i=n1+1; i<n2; ++i)
+    {
+        if(flag == 1)
+            printf("%d ",i);
+            }
+            }
