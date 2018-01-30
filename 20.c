acce08eea0f95e663c6d2e638f2ba80172b2a51b
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-01-30 15:51:28 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf(" the value of a,b");
+scanf(" \n %d%d",&a,&b");
+c=pow(a,b);
+printf("\n the value of %d,a,b);
+getch()
+}
