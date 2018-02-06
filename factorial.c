acd8a9de9dbdf2b160d7905942a2cae1b41a82d9
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-02-06 16:09:33 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int s=1,i,n;
+    clrscr();
+    printf(" the valu of n:");
+    scanf("%d",&n);
+    for(i=1;i<=5;i++)
+    {
+        s=s*i;
+    }
+    printf("%d \n",s);
+}
+getch();
+}
