1733e0f29cca68026e78d284581e08a80b4460fb
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-02-09 16:34:48 +0530

    Create numerical.c

---
+++ numerical.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char n;
+ printf("\n the value of:");
+ scanf("%d",&n);
+ if(n==2)
+ {
+     printf("the value is : numerical");
+ }
+ {
+     printf(" the value is :alphabet");
+ }
+}
