4a4d74fc0906ebbf80e45a0d4005f39c83263bbb
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-02-22 18:31:39 +0800

    Update index.c

--- index.c
+++ index.c
@@ -2,14 +2,14 @@
 #include<conio.h>
 void main()
 {
-    int arr[10],i,n;
+    int a[10],i,n;
 	printf("enter the size of the array:");
 	scanf("%d",&n);
 	printf("\n enter the array elments:");
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&arr[i]);
-		printf("\n%d %d",arr[i],i);
+		scanf("%d",&a[i]);
+		printf("\n%d %d",a[i],i);
 	}
 return 0;
 }
