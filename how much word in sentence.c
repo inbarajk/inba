9f9dcb962c3e638f65fcc6b46c4a2afb5a419712
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-02-22 18:38:02 +0800

    Create how much word in sentence.c

---
+++ how much word in sentence.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+#include <string.h>
+int main()
+{
+char a[100];
+int count=0,i;
+clrscr();
+printf("\n Enter the sentence:");
+scanf("%[^\n]a",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+count++;    
+}
+printf("\n Number of words are: %d",count+1);
+getch();
+return 0;
+}
