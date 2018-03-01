1c2617f6e472e753d3d71d53551afe7eb8e25e02
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-01 15:50:25 +0530

    Update string character.c

--- string character.c
+++ string character.c
@@ -10,7 +10,7 @@ printf("\n Enter the sentence:");
 scanf("%[^\n]a",a);
 for(i=0;a[i]!='\0';i++)
 {
-if(a[i]>='0'&&a[i]<='9')
+if(a[i]>='0'||a[i]<='9')
 count=count+1;    
 }
 printf("\n Number of words are: %d",count);
