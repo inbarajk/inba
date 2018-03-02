b08835127109a140dd171602c85ba031d9f1c26c
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-02 15:09:48 +0530

    Update find the value.c

--- find the value.c
+++ find the value.c
@@ -2,13 +2,7 @@
 #include<conio.h>
 void main()
 {
-    int n,count=0,rev;
-	scanf("%ld",&n);
-	while(n!=0)
-	{
-		rev=n%10;
-		count=count+1;
-		n=n/10;
-	}
-	printf("%ld",count);
+    int n;
+    scanf("%d",&n);
+    printf("the value of: %d",++n);
 }
