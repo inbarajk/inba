922fd9016b28c13544b7db1a5f52b14f9820a967
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-02 14:53:49 +0530

    Create 1 to 10 .c

---
+++ 1 to 10 .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=0,b,i;
+ for(i=0;i<=9;i++)
+ {
+     scanf("%d",&a);
+ }
+ printf("the valu of b:");
+ scanf("%d",&b);
+ if(a&b)
+ {
+     printf("yes",a);
+ }else
+ {
+     printf( "no",b);
+ }
+}
