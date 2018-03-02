b91606ca5a29c0276cd1292b178a82bd78d63f7c
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-02 14:35:34 +0530

    Create not space.c

---
+++ not space.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[200],b[200];
+    int i,j,n1,n2;
+    clrscr();
+    scanf("%s%s",&a[i],&b[j]);
+   
+  {
+      printf("%s",a);
+  
+        printf("%s",b);
+        a[i]=b[j];
+        i++;
+    }
+
+}
