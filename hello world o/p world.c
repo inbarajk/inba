eb2ce26ed35b57a4d4b6c68817442250705e8f25
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-02 14:12:49 +0530

    Create p world.c

---
+++ hello world o/p world.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[200],b[200];
+    int i,j,n1,n2;
+    scanf("%s%s",&a[i],&b[i]);
+    n1=strlen(a);
+    n2=strlen(b);
+    if(n1<n2)
+    {
+        printf("%s",a);
+    }
+    else
+    {
+        printf("%s",b);
+    }
+}
