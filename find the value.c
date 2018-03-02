add64a28449e2af228c4157328d16d6079f1f602
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-02 15:13:07 +0530

    Update and rename find the value.c to next value.c

--- find the value.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-    int n;
-    scanf("%d",&n);
-    printf("the value of: %d",++n);
-}
---
+++ next value.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,c=0,rem=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		n=n/10;
+		c++;
+	}
+	printf("%d",c);
+	return 0;
+}
+
