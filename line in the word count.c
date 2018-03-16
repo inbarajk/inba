cda38f69f13d4583a49f92c2e24eeac6f717991a
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-16 15:04:44 +0530

    Rename line count.c to line in the word count.c

---
+++ line in the word count.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[10];
+    int temp=1,i,b;
+    scanf("%s",a);
+    b=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]=='')
+        printf("%d",temp);
+            }
+            else
+            {
+                printf("%d",temp);
+            }
+            return 0;
+}
