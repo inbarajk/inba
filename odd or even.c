7144f56a09d930e71ea8ada9f01664bfb65a512a
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-16 14:10:49 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    if(c%2==0)
+    {
+        printf("odd");
+    }else
+    {
+        printf("even");
+    }
+}
