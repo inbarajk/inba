eaf3311b361e42bd8807568593cb31a9f918e3ec
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-16 14:56:31 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i,n;
+    scanf("%d",&a);
+    if(a=='1'||a=='0')
+    {
+        b='1';
+    }
+    else
+    {
+        b='0';
+        break;
+    }
+    if(b==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+}
