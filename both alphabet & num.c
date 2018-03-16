df17802c421d9a702d1732a2e504678e85b26a7a
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-16 14:27:37 +0530

    Create both alphabet & num.c

---
+++ both alphabet & num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[50];
+    scanf("%s",&a);
+    if(a==0)
+    {
+        printf("no");
+    }
+    else
+    {
+        printf("yes");
+        
+    }
+    
+    
+}
