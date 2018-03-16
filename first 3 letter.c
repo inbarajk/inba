511354e9d1e7da5034bf1bf14bf2e6babaab30fc
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-16 14:38:54 +0530

    Create first 3 letter.c

---
+++ first 3 letter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[50];
+    int i,n;
+    scanf("%s%d",a,&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+        
+    }
+    
+    
+    
+}
