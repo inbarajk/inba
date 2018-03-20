4c63593ff87b17362e2b20463fe9dc9a6c4dfa36
inbarajk <35652393+inbarajk@users.noreply.github.com>
2018-03-20 16:35:19 +0530

    Create find inbetween value.c

---
+++ find inbetween value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int x=3;
+    if(x>2&&x<6)
+    {
+        printf("yes");
+    }else
+    {
+        printf("no");
+    }
+}
